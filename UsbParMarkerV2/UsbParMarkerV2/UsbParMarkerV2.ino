d849915e415cdea17680dc7ba94a5f161496dfbe
LabsupportEvert <98744988+LabsupportEvert@users.noreply.github.com>
2022-05-18 09:27:28 +0200

    Added cad and bew sw version

--- UsbParMarkerV2/UsbParMarkerV2/UsbParMarkerV2.ino
+++ UsbParMarkerV2/UsbParMarkerV2/UsbParMarkerV2.ino
@@ -1,8 +1,9 @@
 /*
-  Usb Parallel Marker Versie 1 Build 1 10-1-2018
-  Copyright Leiden University - SOLO
+  Usb Parallel Marker Versie 
+  Copyright Leiden University SOLO - Evert 2018~2022
 20180110  1.0 Initial release
-20220214  1.1 Added command mode to make it possible for reading serial number
+20220214  1.1 Added command mode to make it possible for reading serial number, from this version also SOLO qr sticker needs to be applied
+20220518  1.2 Added hardware version to VERSION for upcoming futures
 */
 
 /* IMPORTANT NOTE
@@ -18,14 +19,14 @@
 #include <EEPROM.h>
 
 //Globals
-const String Version = "1.1";
+const String Version = "HW1:SW1.2";
 const String Serialno;
 
 void setup() {
   DDRD = 0xFF; //PortD all pins output
   PORTD = 0x00;
   Serial.begin(115200);     // opens serial port, sets data rate to 115200 bps
-  //writeStringToEEPROM(10, "S01018");    //Use ones to program te serial number in the device
+  //writeStringToEEPROM(10, "S01062");    //Use ones to program te serial number in the device
   Serialno = readStringFromEEPROM(10);
 }
 
